f50300a42b5878d400dc54d6a5d8b46f481feb3a
Omar Solano <omar@designstripe.com>
2024-04-16 19:40:31 -0400

    set force_build to true

--- scripts/get_chunks_modal.py
+++ scripts/get_chunks_modal.py
@@ -1,13 +1,13 @@
 from modal import asgi_app, Image, Stub, Secret, Mount
 
-stub = Stub("chunks-GES824")
+stub = Stub("chunks-ATS900")
 
 image = (
-    Image.debian_slim(force_build=False)
+    Image.debian_slim(force_build=True)
     .apt_install("git")
     .pip_install(
         "-U",
-        "pydantic>=2.6",
+        "pydantic>=2.7",
         "fastapi",
         "uvicorn",
         "numpy",
@@ -21,7 +21,7 @@ image = (
     image=image,
     secrets=[Secret.from_name("my-personal-secrets")],
     mounts=[
-        Mount.from_local_dir("data/GES824/embeds", remote_path="/embeds"),
+        Mount.from_local_dir("data/ATS900/embeds", remote_path="/embeds"),
         Mount.from_local_python_packages("get_chunks_api"),
     ],
     keep_warm=0,
