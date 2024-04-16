0570a9bcc9bcd5fe348044dccb193f83eaa68d48
Omar Solano <omar@designstripe.com>
2024-04-16 19:38:14 -0400

    use cohere rerank

--- scripts/get_chunks_api.py
+++ scripts/get_chunks_api.py
@@ -13,12 +13,14 @@ from dotenv import load_dotenv
 load_dotenv()
 api_key: str | None = os.getenv("COHERE_API_KEY")
 
-PDF_EMBEDS_DATASET = np.load("/embeds/embeddings_GES824.npy")
-PDF_CONTENTS_FILE_PATH = "/embeds/summaries_GES824.jsonl"
+COURS = "ATS800"
+
+PDF_EMBEDS_DATASET = np.load(f"/embeds/embeddings_{COURS}.npy")
+PDF_CONTENTS_FILE_PATH = f"/embeds/summaries_{COURS}.jsonl"
 
 # ONLY FOR AUDIO
-AUDIO_CONTENTS_FILE_PATH = "/embeds/summaries_audio_GES824.jsonl"
-AUDIO_EMBEDS_DATASET = np.load("/embeds/embeddings_audio_GES824.npy")
+AUDIO_CONTENTS_FILE_PATH = f"/embeds/summaries_audio_{COURS}.jsonl"
+AUDIO_EMBEDS_DATASET = np.load(f"/embeds/embeddings_audio_{COURS}.npy")
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -43,8 +45,8 @@ def load_jsonl(file_path):
     return data
 
 
-contents = load_jsonl(PDF_CONTENTS_FILE_PATH)
-audio_contents = load_jsonl(AUDIO_CONTENTS_FILE_PATH)
+list_jsonl_lines: list[dict] = load_jsonl(PDF_CONTENTS_FILE_PATH)
+list_jsonl_lines_audio: list[dict] = load_jsonl(AUDIO_CONTENTS_FILE_PATH)
 
 
 def cosine_similarity_matrix(vectors, query_vec):
@@ -94,10 +96,23 @@ async def retrieve_chunks(user_input: str = ""):
         print(e)
 
     sorted_indices_pdf = np.argsort(similarity_results_pdf)[::-1]
-    relevant_contents_pdf: list = [contents[i] for i in sorted_indices_pdf[:5]]
+    relevant_contents_pdf: list = [list_jsonl_lines[i] for i in sorted_indices_pdf[:10]]
+    logger.info(f"Similarity search completed.")
 
     for d in relevant_contents_pdf:
         d.pop("image_path", None)
+
+    list_texts_chunks = [d["content"] for d in relevant_contents_pdf]
+    async with cohere.AsyncClient(api_key) as co:
+        response = await co.rerank(
+            model="rerank-multilingual-v3.0",
+            query=user_input,
+            documents=list_texts_chunks,
+            top_n=5,
+        )
+    reranked_indexes = [d.index for d in response.results]
+    relevant_contents_pdf = [relevant_contents_pdf[i] for i in reranked_indexes]
+    logger.info(f"Reranking completed.")
 
     # return json.dumps(relevant_contents_pdf, ensure_ascii=False)
 
@@ -115,18 +130,28 @@ async def retrieve_chunks(user_input: str = ""):
 
     sorted_indices_audio = np.argsort(similarity_results_audio)[::-1]
     relevant_contents_audio: list = [
-        audio_contents[i] for i in sorted_indices_audio[:5]
+        list_jsonl_lines_audio[i] for i in sorted_indices_audio[:10]
     ]
+
+    list_audio_texts_chunks = [d["content"] for d in relevant_contents_audio]
+    async with cohere.AsyncClient(api_key) as co:
+        response = await co.rerank(
+            model="rerank-multilingual-v3.0",
+            query=user_input,
+            documents=list_audio_texts_chunks,
+            top_n=5,
+        )
+    reranked_indexes = [d.index for d in response.results]
+    relevant_contents_audio = [relevant_contents_audio[i] for i in reranked_indexes]
+    logger.info(f"Reranking audio completed.")
 
     # Combine PDF and Audio results into one JSON object
     combined_results = {
-        "notes_pdf": relevant_contents_pdf,
+        "pdf_slides": relevant_contents_pdf,
         "audio_transcripts": relevant_contents_audio,
     }
 
-    combined_json = json.dumps(combined_results, ensure_ascii=False)
-
-    return combined_json
+    return json.dumps(combined_results, ensure_ascii=False)
 
 
 class AnswerRequest(BaseModel):
